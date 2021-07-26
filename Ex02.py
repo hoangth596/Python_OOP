c8bac99e62480814a845ad2958d2c9c01d5ead03
hoangth596 <hoang.th586@gmail.com>
2021-07-26 08:58:19 +0700

    Update

--- Ex02.py
+++ Ex02.py
@@ -1,19 +1,42 @@
+import datetime
+import os
+import pandas as pd
+
+def get_index(table_name):
+    file_path = 'Database Exercise 03/' + table_name + '.csv'
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        index = len(pd.read_csv(file_path)) + 1
+    else:
+        index = 1
+
+    return index
+
+
 class Customers:
     """
     This class contain the CustomerID, CustomerName, ContactName,
     Address, City, PostalCode and Country from Customers table
     """
 
-    __id = 0
-    def __init__(self, name, contact, address, city, postal_code, country):
-        Customers.__id += 1
-        self.CustomerId = Customers.__id
-        self.CustomerName = name
-        self.ContactName = contact
-        self.Address = address
-        self.City = city
-        self.Postal_code = postal_code
-        self.Country = country
+    def __init__(self, name, contact, address, city, postal_code, country) -> None:
+        self.__CustomerID = get_index('Customers')
+        self.__validate_type(name, contact, address, city, postal_code, country)
+        self.__CustomerName = name
+        self.__ContactName = contact
+        self.__Address = address
+        self.__City = city
+        self.__Postal_code = postal_code
+        self.__Country = country
+
+
+    @staticmethod
+    def __validate_type(name, contact, address, city, postal_code, country) -> None:
+        assert type(name) == str
+        assert type(contact) == str
+        assert type(address) == str
+        assert type(city) == str
+        assert type(postal_code) == int
+        assert type(country) == str
 
 
 class Employees:
@@ -22,15 +45,23 @@ class Employees:
     BirthDate, Photo and Notes from Employees table
     """
 
-    __id = 0
-    def __init__(self, last_name, first_name, birth_date, photo, notes):
-        Employees.__id += 1
-        self.EmployeeID = Employees.__id
-        self.LastName = last_name
-        self.FirstName = first_name
-        self.BirthDate = birth_date
-        self.Photo = photo
-        self.Notes = notes
+    def __init__(self, last_name, first_name, birth_date, photo, notes) -> None:
+        self.__EmployeeID = get_index('Employees')
+        self.__validate_type(last_name, first_name, birth_date, photo, notes)
+        self.__LastName = last_name
+        self.__FirstName = first_name
+        self.__BirthDate = birth_date
+        self.__Photo = photo
+        self.__Notes = notes
+
+
+    @staticmethod
+    def __validate_type(last_name, first_name, birth_date, photo, notes) -> None:
+        assert type(last_name) == str
+        assert type(first_name) == str
+        assert type(birth_date) == datetime.date
+        assert type(photo) == str
+        assert type(notes) == str
 
 
 class Categories:
@@ -39,12 +70,17 @@ class Categories:
     and Description from Categories table
     """
 
-    __id = 0
-    def __init__(self, name, description):
-        Categories.__id += 1
-        self.CategoryID = Categories.__id
-        self.CategoryName = name
-        self.Description = description
+    def __init__(self, name, description) -> None:
+        self.__CategoryID = get_index('Categories')
+        self.__validate_type(name, description)
+        self.__CategoryName = name
+        self.__Description = description
+
+
+    @staticmethod
+    def __validate_type(name, description) -> None:
+        assert type(name) == str
+        assert type(description) == str
 
 
 class Shippers:
@@ -53,12 +89,17 @@ class Shippers:
     from Shippers table
     """
 
-    __id = 0
     def __init__(self, name, phone):
-        Shippers.__id += 1
-        self.ShipperID = Shippers.__id
-        self.ShipperName = name
-        self.Phone = phone
+        self.__ShipperID = get_index('Shippers')
+        self.__validate_type(name, phone)
+        self.__ShipperName = name
+        self.__Phone = phone
+
+    
+    @staticmethod
+    def __validate_type(name, phone) -> None:
+        assert type(name) == str
+        assert type(phone) == str
 
 
 class Suppliers:
@@ -67,18 +108,29 @@ class Suppliers:
     City, PostalCode, Country and Phone from Suppliers table
     """
 
-    __id = 0
     def __init__(self, name, contact_name, address,
                  city, postal_code, country, phone):
-        Suppliers.__id += 1
-        self.SupplierID = Suppliers.__id
-        self.SupplierName = name
-        self.ContactName = contact_name
-        self.Address = address
-        self.City = city
-        self.PostalCode = postal_code
-        self.Country = country
-        self.Phone = phone
+        self.__SupplierID = get_index('Suppliers')
+        self.__validate_type(postal_code)
+        self.__SupplierName = name
+        self.__ContactName = contact_name
+        self.__Address = address
+        self.__City = city
+        self.__PostalCode = postal_code
+        self.__Country = country
+        self.__Phone = phone
+
+
+    @staticmethod
+    def __validate_type(name, contact_name, address,
+                        city, postal_code, country, phone):
+        assert type(name) == str
+        assert type(contact_name) == str
+        assert type(address) == str
+        assert type(city) == str
+        assert type(postal_code) == int
+        assert type(country) == str
+        assert type(phone) == str
 
 
 class Orders:
@@ -86,15 +138,22 @@ class Orders:
     This class contain OrderID, CustomerID, EmployeeID,
     OrderDate and ShipperID from Orders table
     """
-    
-    __id = 0
+
     def __init__(self, cus_id, employ_id, order_date, shipper_id):
-        Orders.__id += 1
-        self.OrderID = Orders.__id
-        self.CustomerID = cus_id
-        self.EmployeeID = employ_id
-        self.OrderDate = order_date
-        self.ShipperID = shipper_id
+        self.__OrderID = get_index('Orders')
+        self.__validate_type(cus_id, employ_id, order_date, shipper_id)
+        self.__CustomerID = cus_id
+        self.__EmployeeID = employ_id
+        self.__OrderDate = order_date
+        self.__ShipperID = shipper_id
+
+
+    @staticmethod
+    def __validate_type(cus_id, employ_id, order_date, shipper_id):
+        assert type(cus_id) == int
+        assert type(employ_id) == int
+        assert type(order_date) == datetime.date
+        assert type(shipper_id) == int
 
 
 class Products:
@@ -103,15 +162,23 @@ class Products:
     Unit and Price from Products table
     """
 
-    __id = 0
     def __init__(self, product_name, sup_id, cat_id, unit, price):
-        Products.__id += 1
-        self.ProductID = Products.__id
-        self.ProductName = product_name
-        self.SupplierID = sup_id
-        self.CatergoryID = cat_id
-        self.Unit = unit
-        self.Price = price
+        self.__ProductID = get_index('Products')
+        self.__validate_type(product_name, sup_id, cat_id, unit, price)
+        self.__ProductName = product_name
+        self.__SupplierID = sup_id
+        self.__CatergoryID = cat_id
+        self.__Unit = unit
+        self.__Price = price
+
+
+    @staticmethod
+    def __validate_type(product_name, sup_id, cat_id, unit, price):
+        assert type(product_name) == str
+        assert type(sup_id) == int
+        assert type(cat_id) == int
+        assert type(unit) == str
+        assert type(price) == float
 
 
 class OrderDetails:
@@ -120,11 +187,17 @@ class OrderDetails:
     from OrderDetails table
     """
 
-    __id = 0
     def __init__(self, ord_id, prod_id, quantity):
-        OrderDetails.__id += 1
-        self.OderDetailID = OrderDetails.__id
-        self.OrderID = ord_id
-        self.ProductID = prod_id
-        self.Quantity = quantity
+        self.__OderDetailID = get_index('OrderDetails')
+        self.__validate_type(ord_id, prod_id, quantity)
+        self.__OrderID = ord_id
+        self.__ProductID = prod_id
+        self.__Quantity = quantity
+
+
+    @staticmethod
+    def __validate_type(ord_id, prod_id, quantity):
+        assert type(ord_id) == int
+        assert type(prod_id) == int
+        assert type(quantity) == int
 
--- Ex03.py
+++ Ex03.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 from csv import DictWriter, writer
 import Ex02 as data
@@ -11,15 +12,15 @@ def save_info(table_name, class_var):
 
     file_path = 'Database Exercise 03/' + table_name + '.csv'
     info = [val for val in vars(class_var).values()]
-    headers = [key for key in vars(class_var).keys()]
+    headers = [key.replace(f'_{table_name}__', '') for key in vars(class_var).keys()]
 
     if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-        with open(file_path, mode='a') as f:
-            writer_object = writer(f, dialect='excel')
+        with open(file_path, mode='a', newline='') as f:
+            writer_object = writer(f)
             writer_object.writerow(info)
             f.close()
     else:
-        with open(file_path, mode='w') as f:
+        with open(file_path, mode='w', newline='') as f:
             dw = DictWriter(f, delimiter=',', fieldnames=headers)
             dw.writeheader()
             writer_object = writer(f, dialect='excel')
@@ -30,7 +31,7 @@ def save_info(table_name, class_var):
 
 def input_data():
     while True:
-        table = input('Enter the table you want to input data (done to save): ')
+        table = input('Enter the table you want to input data (done to exit): ')
         table = table.capitalize()
         
         if table == 'Customers':
@@ -38,7 +39,13 @@ def input_data():
             contact = input('Enter the customer contact name: ')
             address = input('Enter the customer address: ')
             city = input('Enter the customer city: ')
-            postal_code = input('Enter the city postal code: ')
+            con = False
+            while con == False:
+                try:
+                    postal_code = int(input('Enter the city postal code: '))
+                    con = True
+                except ValueError:
+                    con = False
             country = input('Enter the customer country: ')
             cus = data.Customers(name, contact, address, city, postal_code, country)
             save_info(table, cus)
@@ -46,7 +53,15 @@ def input_data():
         elif table == 'Employees':
             last_name = input('Enter the employee last name: ')
             first_name = input('Enter the employee first name: ')
-            birth_date = input('Enter the employee birth date (DD-MM-YYYY): ')
+            con = False
+            while con == False:
+                birth_date_str = input('Enter the employee birth date (DD-MM-YY): ')
+                try:
+                    birth_date = datetime.strptime(birth_date_str, '%d-%m-%y').date()
+                    con = True
+                except ValueError:
+                    con = False
+                                        
             photo = input('Enter the file name of employee photo: ')
             notes = input('Enter notes for employee: ')
             emp = data.Employees(last_name, first_name, birth_date, photo, notes)
@@ -69,33 +84,34 @@ def input_data():
             contact = input('Enter the supplier contact name: ')
             address = input('Enter the supplier address: ')
             city = input('Enter the supplier city: ')
-            postal_code = input('Enter the city postal code: ')
+            postal_code = input('Enter the city postal code: ').astype(int)
             country = input('Enter the supplier country: ')
             phone = input('Enter the supplier phone number: ')
             sup = data.Suppliers(sup_name, contact, address, city, postal_code, country, phone)
             save_info(table, sup)
 
         elif table == 'Orders':
-            cus_id = input('Enter customer ID: ')
-            emp_id = input('Enter employee ID: ')
-            order_date = input('Enter order date: ')
-            shipper_id = input('Enter shipper ID: ')
+            cus_id = input('Enter customer ID: ').astype(int)
+            emp_id = input('Enter employee ID: ').astype(int)
+            order_date = input('Enter order date (DD-MM-YYYY): ')
+            
+            shipper_id = int(input('Enter shipper ID: '))
             order = data.Orders(cus_id, emp_id, order_date, shipper_id)
             save_info(table, order)
 
         elif table == 'Products':
             prod_name = input('Enter product name: ')
-            sup_id = input('Enter the supplier ID: ')
-            cat_id = input('Enter the category ID: ')
+            sup_id = int(input('Enter the supplier ID: '))
+            cat_id = int(input('Enter the category ID: '))
             unit = input('Enter the product unit: ')
-            price = input('Enter the product price: ')
+            price = float(input('Enter the product price: '))
             product = data.Products(prod_name, sup_id, cat_id, unit, price)
             save_info(table, product)
 
         elif table == 'Orderdetails':
-            order_id = input('Enter the order ID: ')
-            prod_id = input('Enter the product ID: ')
-            quantity = input('Enter the order quantity:')
+            order_id = int(input('Enter the order ID: '))
+            prod_id = int(input('Enter the product ID: '))
+            quantity = int(input('Enter the order quantity:'))
             ord_detail = data.OrderDetails(order_id, prod_id, quantity)
             save_info('OrderDetails', ord_detail)
 
