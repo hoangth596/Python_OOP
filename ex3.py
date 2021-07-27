bf50ac1be1c4cd5ecbfdbe25c3cfbc108e6e0aca
mongvuot336699 <52962271+mongvuot336699@users.noreply.github.com>
2021-07-27 14:12:33 +0700

    Update ex3.py
    Trung Update application

--- ex3.py
+++ ex3.py
@@ -126,3 +126,125 @@ e.read()
 c.save('categorial')
 d.save('supplier')
 """
+
+
+class Application:
+
+    @staticmethod
+    def input_data():
+        while True:
+            table = input('Enter the table you want to input data (done to exit): ')
+            table = table.capitalize()
+            
+            if table == 'Customers':
+                name = input('Enter the customer name: ')
+                contact = input('Enter the customer contact name: ')
+                address = input('Enter the customer address: ')
+                city = input('Enter the customer city: ')
+                postal_code = verify_input('Enter the city postal code: ', int)
+                country = input('Enter the customer country: ')
+                t1 = CustomersTable()
+                t1.add(name, contact, address, city, postal_code, country)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t1.save('Customers')
+                else:
+                    continue
+
+            elif table == 'Employees':
+                last_name = input('Enter the employee last name: ')
+                first_name = input('Enter the employee first name: ')
+                birth_date = verify_date_input('employee birth')
+                photo = input('Enter the file name of employee photo: ')
+                notes = input('Enter notes for employee: ')
+                t2 = EmployeesTable()
+                t2.add(last_name, first_name, birth_date, photo, notes)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t2.save('Employees')
+                else:
+                    continue
+
+            elif table == 'Categories':
+                cat_name = input('Enter the category name: ')
+                descript = input('Enter the category description: ')
+                t3 = CategoriesTable()
+                t3.add(cat_name, descript)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t3.save('Categories')
+                else:
+                    continue
+
+            elif table == 'Shippers':
+                ship_name = input('Enter the shipper name: ')
+                phone = input('Enter the shipper phone number: ')
+                t4 = ShippersTable()
+                t4.add(ship_name, phone)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t4.save('Shippers')
+                else:
+                    continue
+
+
+            elif table == 'Suppliers':
+                sup_name = input('Enter the supplier name: ')
+                contact = input('Enter the supplier contact name: ')
+                address = input('Enter the supplier address: ')
+                city = input('Enter the supplier city: ')
+                postal_code = verify_input('Enter the city postal code: ', int)
+                country = input('Enter the supplier country: ')
+                phone = input('Enter the supplier phone number: ')
+                t5 = SuppliersTable()
+                t5.add(sup_name, contact, address, city, postal_code, country, phone)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t5.save('Suppliers')
+                else:
+                    continue
+
+            elif table == 'Orders':
+                cus_id = verify_input('Enter customer ID: ', int)
+                emp_id = verify_input('Enter employee ID: ', int)
+                order_date = verify_date_input('order')
+                shipper_id = verify_input('Enter shipper ID: ', int)
+                t6 = OrderTable()
+                t6.add(cus_id, emp_id, order_date, shipper_id)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t6.save('Orders')
+                else:
+                    continue
+
+            elif table == 'Products':
+                prod_name = input('Enter product name: ')
+                sup_id = verify_input('Enter the supplier ID: ', int)
+                cat_id = verify_input('Enter the category ID: ', int)
+                unit = input('Enter the product unit: ')
+                price = verify_input('Enter the product price: ', float)
+                t7 = ProductsTable()
+                t7.add(prod_name, sup_id, cat_id, unit, price)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t7.save('Products')
+                else:
+                    continue
+
+            elif table == 'Orderdetails':
+                order_id = verify_input('Enter the order ID: ', int)
+                prod_id = verify_input('Enter the product ID: ', int)
+                quantity = verify_input('Enter the order quantity:', int)
+                t8 = OrderDetailTable()
+                t8.add(order_id, prod_id, quantity)
+                q2 = input('Save file Y/N')
+                if q2 == 'Y':
+                    t8.save('Orderdetails')
+                else:
+                    continue
+
+            elif table.lower() == 'done':
+                exit()
+
+test = Application()
+test.input_data()
